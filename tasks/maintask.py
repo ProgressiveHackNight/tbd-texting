b5944a743f203ccee125e6bf57285d2695934d83
rapicastillo <rapi.castillo@gmail.com>
2018-04-20 13:52:11 -0400

    Main task test

--- tasks/maintask.py
+++ tasks/maintask.py
@@ -1,10 +1,14 @@
 # encoding=utf8
 from django.core.management import call_command
+from subprocess import call
+
 
 def run_pull_data():
     print('RUN] Running Scheduled work: Pull Data');
-    call_command('pull_data')
+    call(["python", "manage.py", "pull_data"])
+
 
 def run_send_confirmation():
     print('RUN] Running Scheduled work: Pull Data');
-    call_command('send_confirmation')
+    # call_command('send_confirmation', interactive=False)
+    call(["python", "manage.py", "send_confirmation"])
---
+++ test.py
@@ -0,0 +1,4 @@
+from tasks import maintask
+
+maintask.run_pull_data()
+maintask.run_send_confirmation()
