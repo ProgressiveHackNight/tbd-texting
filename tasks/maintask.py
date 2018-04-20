439019dce630c2ad3ff06baff948b4c416b33be0
Eric Steinborn <Eric.Steinborn@ag.ny.gov>
2018-04-20 14:12:34 -0400

    send confirmation printout

--- tasks/maintask.py
+++ tasks/maintask.py
@@ -9,6 +9,6 @@ def run_pull_data():
 
 
 def run_send_confirmation():
-    print('RUN] Running Scheduled work: Pull Data');
+    print('RUN] Running Scheduled work: Send Confirmation');
     # call_command('send_confirmation', interactive=False)
     call(["python", "manage.py", "send_confirmation"])
